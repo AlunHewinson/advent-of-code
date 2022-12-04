0f79777e3b711a3b472c9664c25d5835edec96a5
Alun Hewinson <alunhewinson@gmail.com>
2022-12-04 12:01:55 +0000

    solve day 2

--- src/main/scala/aoc2022/day2.scala
+++ src/main/scala/aoc2022/day2.scala
@@ -7,7 +7,19 @@ object day2 extends App {
 
   def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
     val inp: String = readDay(day, test)
-    (98, 98)
+    val games = "\r*\n".r.split(inp)
+    val scoreNaive = Map(
+      "A X" -> 4, "A Y" -> 8, "A Z" -> 3,
+      "B X" -> 1, "B Y" -> 5, "B Z" -> 9,
+      "C X" -> 7, "C Y" -> 2, "C Z" -> 6
+    )
+    val scoreBetter = Map(
+      "A X" -> 3, "A Y" -> 4, "A Z" -> 8,
+      "B X" -> 1, "B Y" -> 5, "B Z" -> 9,
+      "C X" -> 2, "C Y" -> 6, "C Z" -> 7
+    )
+    (games.map(scoreNaive).sum, games.map(scoreBetter).sum)
+    //(98, 98)
   }
   println(solveDay(2, true))
   println(solveDay(2))
