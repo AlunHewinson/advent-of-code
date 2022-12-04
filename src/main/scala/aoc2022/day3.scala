b71c1d3e3a144e6450c03e065c1e8edbdd73f243
Alun Hewinson <alunhewinson@gmail.com>
2022-12-04 15:16:32 +0000

    solve day 3

--- src/main/scala/aoc2022/day3.scala
+++ src/main/scala/aoc2022/day3.scala
@@ -7,16 +7,21 @@ object day3 extends App {
 
   def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
     val inp: String = readDay(day, test)
-    val backpacks = "\r*\n".r.split(inp).map(_.toArray)
+    val backpacks: Array[Array[Char]] = "\r*\n".r.split(inp).map(_.toArray)
     val intersects = backpacks.map(x => {
       val xl: Int = x.length
-      val elf1: Array[Char] = x.slice(0, xl/2)
-      val elf2: Array[Char] = x.slice(xl/2, xl)
+      val elf1: Array[Char] = x.slice(0, xl / 2)
+      val elf2: Array[Char] = x.slice(xl / 2, xl)
       val overlap: Int = (elf1 intersect elf2).map(_.toInt).min
-      (overlap-38) % 58 // evil hack!
+      (overlap - 38) % 58 // evil hack!
     })
-    (intersects.sum, 98)
+    val elfGroups = backpacks.sliding(3, 3).toArray
+    val badges = elfGroups.map(elfGroup => {
+      val badge = elfGroup.reduce((x, y) => x intersect y).min
+      (badge - 38) % 58 // evil hack!
+    })
+    (intersects.sum, badges.sum)
   }
-  println(solveDay(3, true))
-  println(solveDay(3))
+    println(solveDay(3, true))
+    println(solveDay(3))
 }
