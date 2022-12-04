2ce9db153abda37941a91d4373c6453722fc1543
Alun Hewinson <alunhewinson@gmail.com>
2022-12-04 14:39:16 +0000

    'explanatory' note :)

--- src/main/scala/aoc2022/day3.scala
+++ src/main/scala/aoc2022/day3.scala
@@ -13,7 +13,7 @@ object day3 extends App {
       val elf1: Array[Char] = x.slice(0, xl/2)
       val elf2: Array[Char] = x.slice(xl/2, xl)
       val overlap: Int = (elf1 intersect elf2).map(_.toInt).min
-      (overlap-38) % 58
+      (overlap-38) % 58 // evil hack!
     })
     (intersects.sum, 98)
   }
