1d26ad4d57ae7ec235568aa7de4e03827126a19d
Alun Hewinson <alunhewinson@gmail.com>
2022-12-07 23:02:30 +0000

    prep day 5

---
+++ src/main/scala/aoc2022/day5.scala
@@ -0,0 +1,14 @@
+package aoc2022
+
+import utils.utils.readDay
+
+object day5 {
+  // https://adventofcode.com/2022/day/4
+
+  def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
+    (98, 98)
+  }
+  println(solveDay(5, true))
+  println(solveDay(5))
+
+}
