6f476ef45e1e365e9975deb521f17607454b2a5c
Alun Hewinson <alunhewinson@gmail.com>
2022-12-21 18:47:45 +0000

    type annotation on compose

--- src/main/scala/utils/utils.scala
+++ src/main/scala/utils/utils.scala
@@ -16,7 +16,7 @@ object utils {
   def compose[Outer, Inner, Value](
                                     outer: Lens[Outer, Inner],
                                     inner: Lens[Inner, Value]
-                                  ) = Lens[Outer, Value](
+                                  ): Lens[Outer, Value] = Lens[Outer, Value](
     get = outer.get andThen inner.get,
     set = (obj, value) => outer.set(obj, inner.set(outer.get(obj), value))
   )
