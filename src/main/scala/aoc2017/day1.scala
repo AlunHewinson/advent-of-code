47b987aa4e337e9cb1281f335c4899b2b189bcf4
Alun Hewinson <alunhewinson@gmail.com>
2022-12-27 18:32:36 +0000

    solve 2017 day 1

--- src/main/scala/aoc2017/day1.scala
+++ src/main/scala/aoc2017/day1.scala
@@ -9,11 +9,20 @@ object day1 extends App {
   def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
     val inp: String = readDay(day, test, 2017)
     val rowStrings: Seq[String] = "(\r*\n)".r.split(inp).toSeq
+    val captchas: Array[Int] = rowStrings.head.split("").map(_.toInt)
+    val pairs: Iterator[Int] = (captchas :+ captchas.head).sliding(2).map(a => if (a(0) == a(1)) a(0) else 0)
 
-    rowStrings.foreach(println)
+    val podes = captchas.slice(0, captchas.length/2)
+    val antipodes = captchas.slice(captchas.length/2, captchas.length)
+    //podes.foreach(println)
+    //antipodes.foreach(println)
+    val antiPairs = podes.indices.map {i =>
+      println(s"${podes(i)} <-> ${antipodes(i)}")
+      if (podes(i) == antipodes(i)) 2*podes(i) else 0
+    }
 
-    (98, 98)
+    (pairs.sum, antiPairs.sum)
   }
   println(solveDay(day = 1, test = true))
-  println(solveDay(day = 1, test = false))
+  println(solveDay(day = 1, test = false)) //622 too low
 }
