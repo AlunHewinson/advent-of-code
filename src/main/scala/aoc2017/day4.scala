8202ff8726cd8c8f10ec749328054dddd4131140
Alun Hewinson <alunhewinson@gmail.com>
2022-12-28 12:11:58 +0000

    solve 2017 day 4

--- src/main/scala/aoc2017/day4.scala
+++ src/main/scala/aoc2017/day4.scala
@@ -5,13 +5,28 @@ import utils.utils._
 object day4 extends App {
   // https://adventofcode.com/2017/day/4
 
+  def alphabetise(word: String): String = {
+    word.toCharArray.sorted.mkString("")
+  }
+
   def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
     val inp: String = readDay(day, test, 2017)
     val rowStrings: Seq[String] = "(\r*\n)".r.split(inp).toSeq
 
-    rowStrings.foreach(println)
+    val phrases: Seq[Seq[String]] = rowStrings.map(phrase => phrase.split("[ \t]+").toSeq)
 
-    (98, 98)
+    val a1ValidPhrases: Seq[Seq[String]] = phrases.filter(words => words.length == words.toSet.count(_ => true))
+    val a1 = a1ValidPhrases.length
+
+    val tst = phrases.map(words => {
+      val yoyo = words.map(alphabetise)
+      yoyo
+    })
+
+    val a2ValidPhrases = phrases.filter(words => words.length == words.map(alphabetise).toSet.count(_ => true))
+    val a2 = a2ValidPhrases.length
+
+    (a1, a2)
   }
   println(solveDay(day = 4, test = true))
   println(solveDay(day = 4, test = false))
