a835f58bb5eabaf36d12d717206e7fdfacac4cfa
Alun Hewinson <alunhewinson@gmail.com>
2023-01-05 00:46:41 +0000

    solve 2017 day 11

--- src/main/scala/aoc2017/day11.scala
+++ src/main/scala/aoc2017/day11.scala
@@ -5,13 +5,36 @@ import utils.utils._
 object day11 extends App {
   // https://adventofcode.com/2017/day/11
 
+  def hexOffset(offsetString: String): (Int, Int) = {
+    offsetString match {
+      case "n" => (2, 0)
+      case "s" => (-2, 0)
+      case "ne" => (1, 1)
+      case "nw" => (1, -1)
+      case "se" => (-1, 1)
+      case "sw" => (-1, -1)
+    }
+  }
+
+  def addHexCoordinates(hexOffset1: (Int, Int), hexOffset2: (Int, Int)): (Int, Int) = {
+    (hexOffset1._1 + hexOffset2._1, hexOffset1._2 + hexOffset2._2)
+  }
+
+  def hexDistance(hexPoint: (Int, Int)) = {
+    (math.abs(hexPoint._1) - math.abs(hexPoint._2)) / 2 + math.abs(hexPoint._2)
+  }
+
   def solveDay(day: Int, test: Boolean = false): (Int, Int) = {
     val inp: String = readDay(day, test, 2017)
-    val rowStrings: Seq[String] = "(\r*\n)".r.split(inp).toSeq
+    val rowStrings: Seq[String] = "(\r*\n)".r.split(inp).toSeq.head.split(",")
 
-    rowStrings.foreach(println)
+    val currentHex = rowStrings.map(hexOffset).foldLeft((0, 0))(addHexCoordinates) //foreach(println)
+    val a1 = hexDistance(currentHex)
 
-    (98, 98)
+    val scanHex = rowStrings.map(hexOffset).scanLeft((0, 0))(addHexCoordinates)
+    val a2 = scanHex.map(hexDistance).max
+
+    (a1, a2)
   }
   println(solveDay(day = 11, test = true))
   println(solveDay(day = 11, test = false))
